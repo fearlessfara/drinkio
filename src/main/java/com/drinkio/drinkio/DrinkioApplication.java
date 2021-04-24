096125232e271034d020684a78f533188ebd4702
fearlessfara <christiangenn99gmail.com>
2021-04-24 15:33:53 +0200

    initial commit

---
+++ src/main/java/com/drinkio/drinkio/DrinkioApplication.java
@@ -0,0 +1,13 @@
+package com.drinkio.drinkio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/drinkio/drinkio/DrinkioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkio.drinkio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
